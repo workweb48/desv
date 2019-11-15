ae12d80ddfdd628465f7cf9e5548cd22aac16a81
workweb48 <workweb48@gmail.com>
2019-11-16 00:28:09 +0300

    static root

--- desv/settings.py
+++ desv/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 
     os.path.join(BASE_DIR,'static_ev')
 ]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
